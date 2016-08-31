74aab494c97a47dfd114cf8e81fd71261a1b918e
Y-Yanagi <yahyu10101@gmail.com>
2016-08-31 16:23:21 +0900

    Fixed a bug

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -32,7 +32,7 @@ class DetailView(generic.DetailView):
 
 class ResultsView(generic.DetailView):
     model = Question
-    template_name = 'polls/index.html'
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
